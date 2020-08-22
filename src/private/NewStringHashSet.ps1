ca0c9791d2f29f19cd7f6fc810eb3bf43eb945fc
Mike Garvey <mike.garvey@yevrag35.com>
2020-08-22 10:18:25 -0400

    StringHashSet

---
+++ src/private/NewStringHashSet.ps1
@@ -0,0 +1,20 @@
+﻿Function NewStringHashSet() {
+
+    [CmdletBinding()]
+    [OutputType([System.Collections.Generic.HashSet[string]])]
+    param (
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [string[]] $InputObject
+    )
+    Begin {
+        $set = New-Object -TypeName 'System.Collections.Generic.HashSet[string]'
+    }
+    Process {
+        if ($PSBoundParameters.ContainsKey("InputObject")) {
+            $set.UnionWith($InputObject)
+        }
+    }
+    End {
+        , $set
+    }
+}
